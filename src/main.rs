3d49505d13347372d84b440ef6e5de835cae8fe6
Parth Patel <54561512+protocol66@users.noreply.github.com>
2020-11-18 18:13:09 -0600

    Inital Commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+// Halt on panic
+#[allow(unused_extern_crates)] // NOTE(allow) bug rust-lang/rust#53964
+extern crate panic_halt; // panic handler
+
+use cortex_m;
+use cortex_m_rt::entry;
+use stm32f4xx_hal as hal;
+
+use crate::hal::{prelude::*, stm32};
+
+#[entry]
+fn main() -> ! {
+    if let (Some(dp), Some(cp)) = (
+        stm32::Peripherals::take(),
+        cortex_m::peripheral::Peripherals::take(),
+    ) {
+        loop {
+            
+        }
+    }
+
+    loop{} // To ensure program does not leave main
+}
